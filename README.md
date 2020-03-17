d53b5cedf7814f605cb45f3615c975decacae212
Ricardo Imitola <rimitola@mutualser.org>
2020-03-17 10:51:37 -0500

    Creación de servidor en nodejs

---
+++ README.md
@@ -0,0 +1,2 @@
+Web Push App
+Aplicación web utilizando Node.js como servidor, HTML, CSS y JavaScript; y web push como tecnología para enviar notificaciones y service worker.
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const routes = require('./routes')
+const path = require('path')
+const app = express()
+
+// Middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Routes
+app.use(routes)
+
+// Static Content
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+    console.log('Server listening.....')
+})
